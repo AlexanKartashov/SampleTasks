bcdca5bd83753cec0150ea2b222768bfa695951a
Kartashov, Alexander <AKartashov@inwk.com>
2021-04-21 19:57:53 +0300

    add SubArrayIsInArray

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestTask
 {
@@ -10,6 +6,64 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
+            int[] sub = new int[] { 1,6,7,23};
+
+            int[] orig = new int[] {1,6};
+
+            Console.WriteLine(SampleTasks.SampleUtilities.SubArrayIsInArray(sub, orig));
+
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace SampleTasks
+{
+    public static class SampleUtilities
+    {
+        public static bool SubArrayIsInArray(int[] subArray, int[] array)
+        {
+            if ((array.Length == 0) & (subArray.Length != 0))
+                return false;
+
+            if (subArray.Length == 0)
+                return true;            
+            
+            var matchFound = false;
+            var j = 0;
+            for(var i=0;i<array.Length;i++)
+            {
+                if (!matchFound)
+                {
+                    if (array[i] == subArray[0])
+                    {
+                        matchFound = true;
+                    }
+                }
+                else
+                {
+                    j++;
+                    if (subArray.Length == (j+1))
+                    {
+                        if (array[i] == subArray[j])
+                            return true;
+                        else
+                        {
+                            matchFound = false;
+                            j = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (array[i] != subArray[j])
+                        {
+                            matchFound = false;
+                            j = 0;
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
